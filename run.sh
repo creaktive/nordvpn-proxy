11ca9001a3a555e582bcd9dcfd336c4d69e6327c
Stanislaw Pusep <creaktive@gmail.com>
2021-04-04 13:43:19 +0200

    pick a random server out of suggested ones

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ if [ -z "$SERVER" ]; then
     SERVER=$( \
         wget -q -O - 'https://nordvpn.com/wp-admin/admin-ajax.php?action=servers_recommendations' | \
         jq --arg type "openvpn_${PROTOCOL}" -r '.[] | select(.status == "online") | select(.technologies[].identifier == $type) | .hostname' | \
+        shuf | \
         head -n1
     )
 fi
